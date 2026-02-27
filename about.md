c4a8b595d8484796a8ed46f755cdbc5bfa28d69d
MeltyOnIce <-@-.->
2026-02-26 20:58:42 -0800

    I did a lot of stuff

--- about.md
+++ about.md
@@ -1,3 +1,12 @@
-# Template Mod
+# Eye Strain Helper
 
-Edit about.md to change this
+This mod is designed to help prevent eye strain without interrupting your runs like an external tool would.
+
+## Features
+- Optional ability to skip breaks
+- Configurable time between breaks
+- Configurable duration of breaks
+- Configurable option to start breaks when hitting a checkpoint in platformer mode
+- Interoperability with Safe Eyes (incomplete, see WIP)
+
+##
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,94 @@
-/**
- * Include the Geode headers.
- */
+#include "Geode/cocos/cocoa/CCObject.h"
+#include "Geode/cocos/label_nodes/CCLabelBMFont.h"
 #include "Geode/modify/Modify.hpp"
 #include <Geode/Geode.hpp>
+#include <Geode/binding/FLAlertLayer.hpp>
 #include <Geode/binding/GJBaseGameLayer.hpp>
 #include <Geode/binding/GJGameLevel.hpp>
+#include <Geode/binding/GameObject.hpp>
 #include <Geode/binding/LabelGameObject.hpp>
 #include <Geode/binding/PlayLayer.hpp>
 #include <Geode/binding/UILayer.hpp>
+#include <chrono>
+#include <ostream>
+#include <sstream>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
 #include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/UILayer.hpp>
+#include <Geode/binding/AppDelegate.hpp>
+#include <Geode/modify/PlayerObject.hpp>
+
+#include <string>
+
+using std::string;
 
 
 
-class $modify(UILayerMod, UILayer) {
-	
+long calcNow() {
+	return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+}
 
-	bool init(GJBaseGameLayer* layer) {
-		if (!UILayer::init(layer)) {
+const auto modLoad = calcNow();
+auto lastBreak = calcNow();
+
+class $modify(ESHPlayLayer, PlayLayer) {
+	struct Fields {
+		CCNode* eyeStrainHelperUI;
+		CCLabelBMFont* testLabel;
+		long timeLoaded;
+	};
+
+	bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+		if (!PlayLayer::init(level, useReplay, dontCreateObjects)) {
 			return false;
 		}
 
-		FLAlertLayer::create("Eye Strain Helper", "Hello from my custom mod!", "OK")->show();
+		auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+		m_fields->eyeStrainHelperUI = CCNode::create();
+        m_fields->eyeStrainHelperUI->setTag(10420);
+        m_fields->eyeStrainHelperUI->setZOrder(1);
+
+		long now = calcNow();
+		m_fields->timeLoaded = now;
+
+		//std::stringstream labelStr;
+		//labelStr<<"Time since load: "<<now-lastBreak;
+		//m_fields->testLabel = CCLabelBMFont::create(labelStr.str().data(), "bigFont.fnt");
+		//m_fields->testLabel->setPosition({360, winSize.height - 35});
+        //m_fields->testLabel->setScale(0.5F);
+        //m_fields->testLabel->setOpacity(255 / 2);
+		//m_fields->testLabel->setAlignment(CCTextAlignment::kCCTextAlignmentLeft);
+        //m_fields->testLabel->setZOrder(1000);
+        //m_fields->testLabel->setTag(10420); // prevent PlayLayer from interfering
+		//m_fields->testLabel->setVisible(Mod::get()->getSettingValue<bool>("enabled"));
+
+		//m_fields->eyeStrainHelperUI->addChild(m_fields->testLabel);
+		this->addChild(m_fields->eyeStrainHelperUI);
 
 		return true;
 	}
 
+	void pauseGame(bool unfocused) {
+		PlayLayer::pauseGame(unfocused);
+		auto timingAlertEnabled = Mod::get()->getSettingValue<bool>("timingAlertEnabled");
+		if(timingAlertEnabled) {
+			std::stringstream alertContent;
+			long now = calcNow();
+			alertContent<<"Time since epoch: "<<now<<std::endl<<"Time since PlayLayer load: "<<now-m_fields->timeLoaded<<std::endl<<"Time since last break: "<<now-lastBreak<<std::endl<<"Time since mod loaded: "<<now-modLoad;
+			FLAlertLayer::create("Eye Strain Helper", alertContent.str().data(), "OK")->show();
+		}
+	}
+
+	//void destroyPlayer(PlayerObject* player, GameObject* object) {
+	//	auto breakEveryAttempt = Mod::get()->getSettingValue<bool>("breakEveryAttempt");
+	//	PlayLayer::destroyPlayer(player, object);
+	//	if(breakEveryAttempt) {
+	//		FLAlertLayer::create("Eye Strain Helper", "Take a 30 second break! (placeholder)", "OK")->show();
+	//	}
+	//}
 };
+
+class$modify()
