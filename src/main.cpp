44f175ed144815275e8f9a341a3e00216814d9ce
MeltyOnIce <-@-.->
2026-02-26 16:04:25 -0800

    cleared some boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,7 @@ using namespace geode::prelude;
  * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
  */
 #include <Geode/modify/MenuLayer.hpp>
+
 class $modify(MyMenuLayer, MenuLayer) {
 	/**
 	 * Typically classes in GD are initialized using the `init` function, (though not always!),
@@ -41,47 +42,6 @@ class $modify(MyMenuLayer, MenuLayer) {
 		if (!MenuLayer::init()) {
 			return false;
 		}
-
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
-
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
-
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
-
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
-
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
 
 		/**
 		 * We return `true` to indicate that the class was properly initialized.
